f6c189af4d690f516ad206db23a9a76f24480abd
Chhunkea <kimchhunkea64@gmail.com>
2024-04-22 13:07:00 -0700

    New update of nike project

--- Coding/java.js
+++ Coding/java.js
@@ -35,3 +35,34 @@ container.addEventListener('touchmove', (e) => {
   const walk = (x - startX) * 3; 
   container.scrollLeft = scrollLeft - walk;
 });
+
+// click left or right on shoes
+
+var swiper = new Swiper('.swiper-container', {
+  // Your other swiper configuration options...
+
+  // Navigation module
+  navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      hideOnClick: true, // Toggle navigation buttons visibility after click on Slider's container
+  },
+});
+
+// Optional: Add event listeners for navigation events
+swiper.on('navigationHide', function () {
+  console.log('Navigation hidden');
+});
+
+swiper.on('navigationShow', function () {
+  console.log('Navigation shown');
+});
+
+swiper.on('navigationNext', function () {
+  console.log('Next button clicked');
+});
+
+swiper.on('navigationPrev', function () {
+  console.log('Previous button clicked');
+});
+
